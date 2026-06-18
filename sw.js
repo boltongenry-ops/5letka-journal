b603c77e9588ecf976dc99bfdf46411d26c3e9f2
Levokumsky Portal <levokumsky@portal.local>
2026-06-18 20:53:38 +0300

    feat: PWA support (install as app on iPhone/Android)

---
+++ sw.js
@@ -0,0 +1,29 @@
+var CACHE = '5letka-v1';
+var ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(r){
+      var copy = r.clone();
+      caches.open(CACHE).then(function(c){c.put(e.request, copy)});
+      return r;
+    }).catch(function(){
+      return caches.match(e.request);
+    })
+  );
+});
